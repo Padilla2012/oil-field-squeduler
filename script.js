77eabb13e4c2d159b9dfb5fd76cf3c21e48af7a9
Padilla2012 <105744705+Padilla2012@users.noreply.github.com>
2022-07-01 08:49:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-var today = moment();
-	
-	
-	$(document).ready(function(){
+        
+        $(document).ready(function(){
 	$("#currentDay").moment().format("MMMM Do YYYY, h:mm:ss a"); 
-	$("#saveBtn").on("click", function(){
-	var plannerText = $(this).siblings(".description").val();
-	var time = $(this).parent().attr("id");
+	
+        $("#saveBtn").on("click", function(){
+	var plannerText = (this).siblings(".description").val();
+	var time = (this).parent().attr("id");
 	localStorage.setItem(time, plannerText);
 	
 	}); 
 	
-	$("#6 .description").val(localStorage.getItem("6"));
-	$("#7 .description").val(localStorage.getItem("7"));
-	$("#8.description").val(localStorage.getItem("8"));
-        $("#9.description").val(localStorage.getItem("9"));
-	$("#10 .description").val(localStorage.getItem("10"));
-	$("#11 .description").val(localStorage.getItem("11"));
-	$("#12 .description").val(localStorage.getItem("12"));
-	$("#13 .description").val(localStorage.getItem("13"));
-	$("#14 .description").val(localStorage.getItem("14"));
-	$("#15 .description").val(localStorage.getItem("15"));
-	$("#16 .description").val(localStorage.getItem("16"));
-	$("#17 .description").val(localStorage.getItem("17"));
-	$("#18 .description").val(localStorage.getItem("18"));
-	// add more rows for more time
-	function timeChange(){
-	var currentHour = moment().hour(); // Number
+	val(localStorage.getItem("6"));
+	val(localStorage.getItem("7"));
+	val(localStorage.getItem("8"));
+        val(localStorage.getItem("9"));
+	val(localStorage.getItem("10"));
+	val(localStorage.getItem("11"));
+	val(localStorage.getItem("12"));
+	val(localStorage.getItem("13"));
+	val(localStorage.getItem("14"));
+	val(localStorage.getItem("15"));
+	val(localStorage.getItem("16"));
+	val(localStorage.getItem("17"));
+	val(localStorage.getItem("18"));
 	
-	$('.time-block').each(function(){
-	var idTime = parseInt($(this).attr("id"));
+	function timeChange(){
+	var currentHour = moment().hour();
+	
+	$('time-block').each(function(){
+	var idTime = parseInt((this).attr("id"));
 	if (idTime < currentHour){
-	$(this).addClass("past");
+	(this).addClass("past");
 	} else if (idTime === currentHour){
-	$(this).addClass("present");
-	$(this).removeClass("past");
+	(this).addClass("present");
+	(this).removeClass("past");
 	} else {$(this).addClass("future");
-	$(this).removeClass("past");
-	$(this).removeClass("present");
+	(this).removeClass("past");
+	(this).removeClass("present");
 	}
 	})
 	}
