8e25cba7ce07219057c6bd1bf6f7ff56af51179b
Padilla2012 <padilla-silva@hotmail.com>
2022-07-01 00:03:18 -0500

    added script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+var today = moment();
+	
+	
+	$(document).ready(function(){
+	$("#currentDay").moment().format("MMMM Do YYYY, h:mm:ss a"); 
+	$(".saveBtn").on("click", function(){
+	var plannerText = $(this).siblings(".description").val();
+	var time = $(this).parent().attr("id");
+	localStorage.setItem(time, plannerText);
+	
+	}); 
+	
+	$("#6 .description").val(localStorage.getItem("6"));
+	$("#7 .description").val(localStorage.getItem("7"));
+	$("#8.description").val(localStorage.getItem("8"));
+    $("#9.description").val(localStorage.getItem("9"));
+	$("#10 .description").val(localStorage.getItem("10"));
+	$("#11 .description").val(localStorage.getItem("11"));
+	$("#12 .description").val(localStorage.getItem("12"));
+	$("#13 .description").val(localStorage.getItem("13"));
+	$("#14 .description").val(localStorage.getItem("14"));
+	$("#15 .description").val(localStorage.getItem("15"));
+	$("#16 .description").val(localStorage.getItem("16"));
+	$("#17 .description").val(localStorage.getItem("17"));
+	$("#18 .description").val(localStorage.getItem("18"));
+	// add more rows for more time
+	function timeChange(){
+	var currentHour = moment().hour(); // Number
+	
+	$('.time-block').each(function(){
+	var idTime = parseInt($(this).attr("id"));
+	if (idTime < currentHour){
+	$(this).addClass("past");
+	} else if (idTime === currentHour){
+	$(this).addClass("present");
+	$(this).removeClass("past");
+	} else {$(this).addClass("future");
+	$(this).removeClass("past");
+	$(this).removeClass("present");
+	}
+	})
+	}
+	timeChange();
+	var timeInterval = setInterval(timeChange, 1000);
+	
+	
+	});
